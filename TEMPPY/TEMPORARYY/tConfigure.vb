4385c1984f9d3eaf848d2985d986532d55be94df
lamngo13 <lamngo@live.unc.edu>
2022-05-23 13:45:43 -0400

    abt to delete form 1

---
+++ TEMPPY/TEMPORARYY/tConfigure.vb
@@ -0,0 +1,5 @@
+﻿Public Class tConfigure
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+    End Sub
+End Class
--- TEMPPY/TEMPORARYY/temp2vonfig.vb
+++ TEMPPY/TEMPORARYY/temp2vonfig.vb
@@ -1,2 +1,7 @@
 ﻿Public Class temp2vonfig
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim tConfigure As New tConfigure
+        tConfigure.StartPosition = FormStartPosition.CenterScreen
+        tConfigure.ShowDialog()
+    End Sub
 End Class
